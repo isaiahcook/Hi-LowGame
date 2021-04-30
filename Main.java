a739250376f10e38bcfe50bb41cb4b6b3ae30ace
Isaiah Cook <>
2021-04-30 17:40:56 +0000

    updated the Hi-Low-Game code to this repo

--- Main.java
+++ Main.java
@@ -1,34 +1,46 @@
+import java.util.Random;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-/*      Set up a generator for a random integer between 1 and 100 including those two numbers as well
-        Set an int variable equal to that random number
-        Make an if else loop with the nested statements to evaluate if the guess was too low or high
-        We want to make a limited number of guesses only 10 guesses allowed. Using the method from the battleship game.
-        If the 10 guesses are reached the program will completely restart.
-        Give the user the option to quit out of the game which would end the loop. Using an if statement to exit out of the program. This would make them have to start the program over.
-        No decimals only whole numbers
-        
-        print(random.randint())
-        
-        Welcome to the Hi-Low Guessing Game !
-        Pick a number between 1 - 100 ____
-        Your Pick was 25 
-        You are to low
-        Pick a number between 1-100 ____
-        Your Pick was 91
-        You are to high
-        You have used all your guesses, do you want to play again? Y/N
-        You selected N
-        Thanks for playing Hi-Low 
-        
-*/   
-System.out.println("Welcome to the Hi-Low Guessing Game");
-Scanner input = new Scanner (System.in);
-int guesses = 0;
-while (guesses <= 10) {
-    System.out.println("Pick a number between 1-100");
-    guess = input.nextInt();
+
+
+        int guess;
+        int answer;
+        String restart;
+
+        System.out.println(" Welcome to the High-Low Guessing Game.");
+        System.out.println("You'll guess a number, but we will only tell you whether you are too high or too low.");
+        Random generator = new Random();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Pick a number between 1-100(enter 0 to quit)? ");
+        guess = input.nextInt();
+
+        answer = generator.nextInt(100) + 1; // this is what gets a random number between 1-100 each time
+
+        do {
+
+
+            if (guess < answer) {
+                System.out.println("Your guess is too low, try again");
+                guess = input.nextInt();
+            }
+            if (guess > answer) {
+                System.out.println("Your guess is too high, try again");
+                guess = input.nextInt();
+            }
+        }
+        while (guess != 0 && guess <= 10);
+
+        if (guess == 0) { // if the user types 0, the game will quit.
+            System.out.println("\nDo you want to play again Y/N?");
+            restart = input.next();
+            if (restart.equalsIgnoreCase("Y")) {
+                System.out.print("Enter a number between 1 and 100 to start playing");
+                guess = input.nextInt();
+            }
+        }
+        System.out.print("Thanks for playing! I will beat you next time");
     }
-}
 }
